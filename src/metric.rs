8af7b5970405a125cd64fcfd842f23f321cf355c
softprops <d.tangren@gmail.com>
2018-07-08 16:07:15 -0400

    more tests and docs

--- src/metric.rs
+++ src/metric.rs
@@ -1,6 +1,10 @@
 // Std
 use std::time::{SystemTime, UNIX_EPOCH};
 
+/// Return datadog lambda counter metric
+///
+/// See the datadog [docs](https://docs.datadoghq.com/integrations/amazon_lambda/#lambda-metrics)
+/// for more information
 pub fn incr(metric_name: &str, tags: Vec<String>) -> Option<String> {
   SystemTime::now().duration_since(UNIX_EPOCH).ok().map(|d| {
     format!(
@@ -11,4 +15,25 @@ pub fn incr(metric_name: &str, tags: Vec<String>) -> Option<String> {
       tags = tags.join(",")
     )
   })
+}
+
+#[cfg(test)]
+mod tests {
+  use super::incr;
+
+  #[test]
+  fn without_tags() {
+    let metric = incr("foo.bar", Vec::new()).expect("time is moving backwards");
+    let parts = metric.split("|").collect::<Vec<_>>();
+    assert_eq!(parts[4], "foo.bar");
+  }
+
+  #[test]
+  fn with_tags() {
+    let metric =
+      incr("foo.bar", vec!["baz:boom".into(), "zoom".into()]).expect("time is moving backwards");
+    let parts = metric.split("|").collect::<Vec<_>>();
+    assert_eq!(parts[4], "foo.bar");
+    assert_eq!(parts[5], "#baz:boom,zoom");
+  }
 }
